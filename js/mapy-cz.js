e13fda9972980c387f342f7c4f62512106129d6f
Matyáš Bach <matyas.bach@plus4u.net>
2018-12-09 00:00:39 +0100

    Support all kinds of WGS84 formats #4

--- js/mapy-cz.js
+++ js/mapy-cz.js
@@ -1,46 +1,46 @@
-jQuery( document ).ready( function ( $ ) {
-	$.post(
-		mcz_map_ajax.ajaxurl,
-		{
-			action: 'mapy-cz-get-map',
-			maps:   mcz_load_maps
-		},
-		function ( response) {
-			var map_id;
-			Loader.async = true;
-			Loader.load( null, { poi: true }, function() {
-				var maps = $.parseJSON( response );
+jQuery(document).ready(function($) {
+  $.post(
+    mcz_map_ajax.ajaxurl,
+    {
+      action: "mapy-cz-get-map",
+      maps: mcz_load_maps
+    },
+    function(response) {
+      var map_id;
+      Loader.async = true;
+      Loader.load(null, { poi: true }, function() {
+        var maps = $.parseJSON(response);
 
-				for ( map_id in maps ) {
-					var center = SMap.Coords.fromWGS84(
-						parseFloat( maps[ map_id ].lat ),
-						parseFloat( maps[ map_id ].lon )
-					),
-					map = new SMap( JAK.gel( map_id ), center, 15 );
+        for (map_id in maps) {
+          var center = SMap.Coords.fromWGS84(
+              maps[map_id].lat,
+              maps[map_id].lon
+            ),
+            map = new SMap(JAK.gel(map_id), center, 15);
 
-					map.addDefaultLayer( SMap.DEF_SMART_BASE ).enable();
-					map.addDefaultControls();
-					map.addControl( new SMap.Control.Sync( { bottomSpace: 0 } ) );
-					
-					var layer = new SMap.Layer.Marker();
-					map.addLayer( layer ).enable();
-					
-					var marker = new SMap.Marker( center, "myMarker"/*, options*/ );
-					layer.addMarker( marker );
+          map.addDefaultLayer(SMap.DEF_SMART_BASE).enable();
+          map.addDefaultControls();
+          map.addControl(new SMap.Control.Sync({ bottomSpace: 0 }));
 
-					var poiLayer = new SMap.Layer.Marker();
-					map.addLayer( poiLayer ).enable();
+          var layer = new SMap.Layer.Marker();
+          map.addLayer(layer).enable();
 
-					var dataProvider = map.createDefaultDataProvider();
-					dataProvider.setOwner( map );
-					dataProvider.addLayer( poiLayer );
-					dataProvider.setMapSet( SMap.MAPSET_BASE );
-					dataProvider.enable();
+          var marker = new SMap.Marker(center, "myMarker" /*, options*/);
+          layer.addMarker(marker);
 
-					$( "#" + map_id ).data( "map", map );
-				}
-			});
-		}
-	);
-	return false;
+          var poiLayer = new SMap.Layer.Marker();
+          map.addLayer(poiLayer).enable();
+
+          var dataProvider = map.createDefaultDataProvider();
+          dataProvider.setOwner(map);
+          dataProvider.addLayer(poiLayer);
+          dataProvider.setMapSet(SMap.MAPSET_BASE);
+          dataProvider.enable();
+
+          $("#" + map_id).data("map", map);
+        }
+      });
+    }
+  );
+  return false;
 });
